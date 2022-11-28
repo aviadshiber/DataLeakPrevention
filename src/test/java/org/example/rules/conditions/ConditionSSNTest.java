74e1ff071a822505dfdccee2297be553c6d773d3
aviadshiber <aviadshiber@alumni.technion.ac.il>
2022-11-28 21:30:31 +0200

    fix test

--- src/test/java/org/example/rules/conditions/ConditionSSNTest.java
+++ src/test/java/org/example/rules/conditions/ConditionSSNTest.java
@@ -14,13 +14,13 @@ public class ConditionSSNTest extends TestCase {
     }
     public void testSSNSadPath(){
         val invalidSSN = List.of(
-                "000-45-6789",
-                "666-45-6789",
-                "901-45-6789",
-                "85-345-6789",
-                "856-453-6789",
-                "856-45-67891",
-                "856-456789"
+                "0000 45-6789",
+                "666-453-67890",
+                "901-450-6789",
+                "85-345-06789",
+                "856-453-06789",
+                "856-45-6",
+                "856-4567890"
         );
         assert invalidSSN.stream().noneMatch(condition);
     }
